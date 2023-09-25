51ba9c391eb8f950aaa2d24775128a4067419329
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-25 17:10:56 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, attribute) {
+ let newDrivers = drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+ return newDrivers;
+  }
+  
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+   let matchedNames = drivers.filter(function (driver) { return driver.name === argument })
+   return matchedNames;
+  }
